663b6ba588d92c72ec243d42986b2a86660132cd
ichsanHPM <ichsanhpm@gmail.com>
2024-12-01 18:40:54 +0700

    menambahkan colo dan row

--- app/src/main/java/com/example/questnavigasimvvm/ui/theme/view/FormulirView.kt
+++ app/src/main/java/com/example/questnavigasimvvm/ui/theme/view/FormulirView.kt
@@ -1,2 +1,110 @@
 package com.example.questnavigasimvvm.ui.theme.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormulirView(
+    modifier: Modifier = Modifier,
+    listJK: List<String>,
+    onSubmitClicked: (MutableList<String>) ->Unit
+){
+    var nama by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var nim by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var notelpon by remember { mutableStateOf("") }
+
+    val listData : MutableList<String> = mutableListOf(nama, gender, email, nim, alamat, notelpon)
+
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = nama,
+            onValueChange = { nama = it },
+            label = {
+                Text(text = "Nama")
+            },
+            placeholder = {
+                Text(text = "Isi Nama Anda")
+            },
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+        )
+        Row (modifier = Modifier.fillMaxWidth()){
+            listJK.forEach{selectedGender->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = gender == selectedGender,
+                        onClick = {gender = selectedGender}
+                    )
+                    Text(text = selectedGender)
+                } }
+        }
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = {
+                Text(text = "Email")
+            },
+            placeholder = {
+                Text(text = "Isi Email Anda")
+            },
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+        TextField(
+            value = nim,
+            onValueChange = { nim = it },
+            label = {
+                Text(text = "Nim")
+            },
+            placeholder = {
+                Text(text = "Isi Nim Anda")
+            },
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        TextField(
+            value = alamat,
+            onValueChange = { alamat = it },
+            label = {
+                Text(text = "Alamat")
+            },
+            placeholder = {
+                Text(text = "Isi Alamat Anda")
+            },
+            modifier = Modifier.fillMaxWidth().padding(5.dp)
+        )
+        TextField(
+            value = notelpon,
+            onValueChange = { notelpon = it },
+            label = {
+                Text(text = "No Telpon")
+            },
+            placeholder = {
+                Text(text = "Isi No Telpon Anda")
+            },
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+
+    }
+}

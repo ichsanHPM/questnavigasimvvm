3156d7c5b273f098a7d181fb26e4a5db7af6ca33
ichsanHPM <ichsanhpm@gmail.com>
2024-12-01 18:07:04 +0700

    penambahan object jenis kelamin

---
+++ app/src/main/java/com/example/questnavigasimvvm/model/JenisKelamin.kt
@@ -0,0 +1,11 @@
+package com.example.questnavigasimvvm.model
+
+import com.example.questnavigasimvvm.R
+
+object JenisKelamin {
+
+    val jenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}

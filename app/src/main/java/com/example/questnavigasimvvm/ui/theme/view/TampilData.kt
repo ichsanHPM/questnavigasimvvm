c3dbc09788f7bfbda95fd28b0754d54746fc2254
ichsanHPM <ichsanhpm@gmail.com>
2024-12-01 18:52:32 +0700

    menambahkan column dan row

--- app/src/main/java/com/example/questnavigasimvvm/ui/theme/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasimvvm/ui/theme/view/TampilData.kt
@@ -1,7 +1,10 @@
 package com.example.questnavigasimvvm.ui.theme.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -33,5 +36,18 @@ fun TampilDataView(
 
 @Composable
 fun TampilData(param: String, argu: String) {
+
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = param, modifier = Modifier.weight(0.8f))
+            Text(text = ": ", modifier = Modifier.weight(0.2f))
+            Text(text = argu, modifier = Modifier.weight(2f))
+        }
+    }
 
 }

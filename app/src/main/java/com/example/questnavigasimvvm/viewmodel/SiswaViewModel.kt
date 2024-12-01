f40fefefeba2484cd1ec9083bdc4127ed9fd2ab4
ichsanHPM <ichsanhpm@gmail.com>
2024-12-01 19:00:45 +0700

    membuat funtion saveDataSiswa

--- app/src/main/java/com/example/questnavigasimvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/questnavigasimvvm/viewmodel/SiswaViewModel.kt
@@ -1,4 +1,28 @@
 package com.example.questnavigasimvvm.viewmodel
 
-class SiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.questnavigasimvvm.model.DataSiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+
+    private val _statusUI = MutableStateFlow(DataSiswa())
+
+    val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
+
+    fun saveDataSiswa(ls: MutableList<String>){
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                gender = ls[1],
+                email = ls[2],
+                nim = ls[3],
+                alamat = ls[4],
+                notelpon = ls[5]
+            )
+        }
+    }
 }

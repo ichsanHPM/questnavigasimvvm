fad8d6c9db56035085d28c93e0e6b9d8d08943c2
ichsanHPM <ichsanhpm@gmail.com>
2024-12-01 18:07:04 +0700

    penambahan data class

---
+++ app/src/main/java/com/example/questnavigasimvvm/model/DataSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.questnavigasimvvm.model
+
+data class DataSiswa(
+
+    val nama : String="",
+    val gender : String="",
+    val email : String="",
+    val nim : String="",
+    val alamat : String="",
+    val notelpon : String=""
+)
